1b288d5ff0cedc70f37eff9881ab1b32ff37edb1
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-04-22 20:46:47 -0400

    edditing html and js

---
+++ assets/javaScript/script.js
@@ -0,0 +1,27 @@
+var start = document.getElementById('start')
+var questions = document.querySelector('.options')
+var 
+var firstA = document.getElementById('firstA')
+var secondB = document.getElementById('secondB')
+var thirdC = document.getElementById('thirdC')
+var fourthD = document.getElementById('fourthD')
+
+
+var codeQuestion = [
+    {
+     questions: "What is JS?",
+        firstA: "Javafx",
+        secondB: "Java Script",
+        thirdC:  "Java",
+        fourthD: "C++",
+        answer: "secondB"
+    },
+    //Second set of questions 
+    {
+        questions: "Do you like JS?",
+        firstA: "no",
+        secondB: "yes",
+        thirdC:  "idk",
+        fourthD: "yessss"
+    }
+]
